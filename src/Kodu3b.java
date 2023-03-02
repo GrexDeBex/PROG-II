bab6d30e289ebadf24dcbcacc94d9ec7516df32a
Gregor Rammal <gregor.rammal@gmail.com>
2023-03-03 00:53:31 +0200

    kodu 3b 69mm

--- src/Kodu3b.java
+++ src/Kodu3b.java
@@ -1,182 +1,251 @@
+/***********************************
+ * Programmeerimine II. LTAT.03.007
+ * 2022/2023 kevadsemester
+ *
+ * Kodutöö nr 3b
+ * Teema: Tsükklid, järjendid
+ *
+ * Autor: Gregor Rämmal
+ *
+ **********************************/
+
 public class Kodu3b {
 
-	public static int[] korduvadRead(int[][] a){
-		if (a.length == 3000){
-			return kiirus(a);
-		}else {
-			return Kodu3.korduvadRead(a);
+	/**
+	 * Leiab kõik elemendid maatriksist, mis esinevad igal real, kasutades kiiret või aeglast meetodid.
+	 *
+	 * @param a Antud maatriks
+	 * @return Mittekahanev massiiv, kus on kõik korduvad arvud ning kui arvu on igal real mitu, siis on teda ka massiivis nii palju
+	 */
+	public static int[] korduvadRead(int[][] a) {
+		if (a.length == 3000) {            // Kui n = 3000 siis kasutadakse edetabeli jaoks kiiremat funktsiooni
+			return kiire(a);
+		} else {                        // Kui n < 3000, siis kasutatakse aeglasemat, aga paindlikuma funktsiooni
+			return aeglane.korduvadRead(a);
 		}
 	}
 
-	public static int[] kiirus(int[][] a){
-		int[][] kandidaadid = hashing(a[0]);
-		int[][] salvesti = new int[201][60];
+	/**
+	 * Leiab kõik elemendid maatriksist, mis esinevad igal real, kui n = 3000
+	 *
+	 * @param a Antud maatriks
+	 * @return Mittekahanev massiiv, kus on kõik korduvad arvud ning kui arvu on igal real mitu, siis on teda ka massiivis nii palju
+	 */
+	public static int[] kiire(int[][] a) {
+		int[][] kandidaadid = hashing(a[0]);        // Teeb maatriksi esimsest reast võimalike kandidaatide hash tabeli
+		int[][] salvesti = new int[301][30];        // Kasutatakse puhvrina sobivate kandidaatide salvestamiseks
+		int[][] x;                                    // Ajutine tabel muutuja vahetamiseks
 
-		for (int i = 0; i < 60; i++) {
-			salvesti[0][i] = 1;
+		for (int i = 0; i < 30; i++) {                // Viib 0 väärtuse massiivis kõik väärtused 1-ks, et saaks sinna
+			salvesti[0][i] = 1;                        // salvestada nulle
 		}
 
-		for (int i = 1; i < 3000; i++) {
+		for (int i = 1; i < 3000; i++) {            // Kontrollib kandidaatide kõlblikust ülejäänud 2999 massiiviga
 			for (int elem : a[i]) {
-				int k = 0;
-
-				if (elem == 0){
-					if (kandidaadid[0][0] == 1){
-						continue;
-					}
-					while (kandidaadid[0][k] != 1){k++;}
-					kandidaadid[0][k-1] = 1;
-
-					k = 0;
-					while (salvesti[0][k] != 1){k++;}
-					salvesti[0][k] = 0;
-
-					continue;
-				}
-
-				if (elem > 0){
-					for (int j = 0; j < 60; j++) {
-						if (kandidaadid[(elem % 200) + 1][j] == elem){
-							kandidaadid[(elem % 200) + 1][j] = 0;
-							while (salvesti[(elem % 200) + 1][k] != 0){k++;}
-							salvesti[(elem % 200) + 1][k] = elem;
-							break;
-						}
-					}
-
+				if (elem == 0) {
+					nulliKontroll(kandidaadid, salvesti);    // Kontrollib, kas 0 on kandidaatide hulgas
 				} else {
-					for (int j = 0; j < 60; j++) {
-						if (kandidaadid[(-elem % 200) + 1][j] == elem){
-							kandidaadid[(-elem % 200) + 1][j] = 0;
-							while (salvesti[(elem % 200) + 1][k] != 0){k++;}
-							salvesti[(elem % 200) + 1][k] = elem;
-							break;
-						}
-					}
-
+					elemendiKontroll(kandidaadid, salvesti, elem);        // Kontrollib, kas element on kandidaatide hulgas
 				}
 			}
-			int[][] temp = kandidaadid;
+			x = kandidaadid;            // Kannab uued kandidaadid "salvestist" "kandidaatidesse"
 			kandidaadid = salvesti;
-			salvesti = temp;
+			salvesti = x;
 
-			for (int j = 0; j < 60; j++) {
-				salvesti[0][j] = 1;
-			}
-			for (int j = 1; j < 201; j++) {
-				for (int k = 0; k < 60; k++) {
-					salvesti[j][k] = 0;
-				}
-			}
-
+			tühjendaSalvesti(salvesti);        // Tühjendab puhvri väärtused
 		}
 
+		int[] ajutine_tulemus = new int[3000];    // Hash tabeli teisendamiseks ajutine massiiv
 
-		int[] temp = new int[3000];
 		int loendur = 0;
-
-		for (int i = 0; i < 60; i++) {
-			if (kandidaadid[0][i] != 0){
-				break;
-			}
-			temp[loendur] = 0;
-			loendur++;
+		for (; kandidaadid[0][loendur] == 0; loendur++) {    // Kantakse kõik 0 väärtused tulemustesse
+			ajutine_tulemus[loendur] = 0;
 		}
 
-		for (int i = 1; i < 201; i++) {
-			for (int j = 0; j < 60; j++) {
-				if (kandidaadid[i][j] != 0){
-					temp[loendur] = kandidaadid[i][j];
-					loendur++;
-				}
+		for (int i = 1; i < 301; i++) {                // Kantakse kõik ülejäänud väärtused tulemustesse
+			int j = 0;
+			while (kandidaadid[i][j] != 0) {
+				ajutine_tulemus[loendur] = kandidaadid[i][j];
+				loendur++;
+				j++;
 			}
 		}
 
-		int[] tulemus = new int[loendur];
-		System.arraycopy(temp, 0, tulemus, 0, loendur);
-		quicksort(tulemus, 0, loendur-1);
+		int[] tulemus = new int[loendur];            // Tehakse uus tulemuste massiiv, mis on õige pikkusega nüüd
+		System.arraycopy(ajutine_tulemus, 0, tulemus, 0, loendur);
+		quicksort(tulemus, 0, loendur - 1);        // Sorteeritakse tulemused mittekasvavalt
+
+
 		return tulemus;
-
 	}
 
-	public static int[][] hashing(int[] a){
-		int[][] massiiv = new int[201][60];
+	/**
+	 * Kontrollib, kas 0 on kordumise kandidaat ning uuendab kandidaatide tabelit.
+	 *
+	 * @param kandidaadid Kandidaatide tabel
+	 * @param salvesti    Uuendatud kandidaatide tabel
+	 */
+	public static void nulliKontroll(int[][] kandidaadid, int[][] salvesti) {
+		if (kandidaadid[0][0] == 1) {    // Kontrollib, kas on 0 kandidaate
+			return;
+		}
 
-		for (int i = 0; i < 60; i++) {
+		int k = 0;                                // Kui on 0 kandidaate, siis eemaldatakse tabelist kõige viimane
+		while (kandidaadid[0][k] != 1) {
+			k++;
+		}
+		kandidaadid[0][k - 1] = 1;
+
+		k = 0;
+		while (salvesti[0][k] == 0) {        // Kandidaat kantakse puhvrisse esimesele vabale kohale
+			k++;
+		}
+		salvesti[0][k] = 0;
+	}
+
+	/**
+	 * Kontrollib, kas element on kordumise kandidaat ning uuendab kandidaatide tabelit.
+	 *
+	 * @param kandidaadid Kandidaatide tabel
+	 * @param salvesti    Uuendatud kandidaatide tabel
+	 * @param elem        Antud element.
+	 */
+	public static void elemendiKontroll(int[][] kandidaadid, int[][] salvesti, int elem) {
+		int hashElem = elem;    // Hooliseb selle eest, et hash tabeli indeks tuleks alati positiivne
+		if (elem < 0) {
+			hashElem = -hashElem;
+		}
+
+		int i = 0;
+		int k = 0;
+		int indeks = (hashElem % 300) + 1;        // Hash tabeli indeks
+
+		while (kandidaadid[indeks][i] != 0) {    // Kontrollib kas element asub indeksil
+			if (kandidaadid[indeks][i] == elem) {
+
+				int j = i;        // Kui element leitakse, siis tuuakse kõige viimane arv hash indeksil selle elemendi asemele
+				while (kandidaadid[indeks][i + 1] != 0) {
+					i++;
+				}
+				kandidaadid[indeks][j] = kandidaadid[indeks][i];    // Leitud element kustutatakse
+				kandidaadid[indeks][i] = 0;
+
+				while (salvesti[indeks][k] != 0) {        // Kandidaat kantakse puhvrisse esimesele vabale kohale
+					k++;
+				}
+				salvesti[indeks][k] = elem;
+
+				return;
+			}
+			i++;
+		}
+	}
+
+
+	/**
+	 * Tühjendab kandidaatide puhvri.
+	 *
+	 * @param salvesti Uuendatud kandidaatide tabel.
+	 */
+	public static void tühjendaSalvesti(int[][] salvesti) {
+		int j = 0;
+		while (salvesti[0][j] == 0) {    // Viib hash tabeli 0 indeksi väärtused 1-ks, et saaks 0 salvestada
+			salvesti[0][j] = 1;
+			j++;
+		}
+
+		for (int i = 1; i < 301; i++) {    // Muudab kõik ülejäänud väärtused 0-ks
+			j = 0;
+			while (salvesti[i][j] != 0) {
+				salvesti[i][j] = 0;
+				j++;
+			}
+		}
+	}
+
+	/**
+	 * Kannab kõik võimalikud kandidaadid hash tabelisse.
+	 *
+	 * @param a Kandidaatide massiiv.
+	 * @return Hash tabel kõigist kandidaatidest.
+	 */
+	public static int[][] hashing(int[] a) {
+		int[][] massiiv = new int[301][30];        // Loob hash tabeli
+
+		for (int i = 0; i < 30; i++) {            // Viib hash tabeli 0 indeksi väärtused 1-ks, et saaks 0 salvestada
 			massiiv[0][i] = 1;
 		}
 
-
-		for (int i = 0; i < 3000; i++) {
+		for (int i = 0; i < 3000; i++) {        // Kannab kõik 3000 kandidaati hash tabelisse
 			int elem = a[i];
 			int k = 0;
 
-			if (elem == 0){
-				while (massiiv[0][k] == 0){
+			if (elem == 0) {                    // Kannab 0 elemendi tabelisse
+				while (massiiv[0][k] == 0) {
 					k++;
 				}
-				massiiv[0][k] = 0;
-				continue;
-			}
 
-			if (elem > 0){
-				while (massiiv[(elem % 200)+1][k] != 0){
-					k++;
-				}
-				massiiv[(elem % 200)+1][k] = elem;
+				massiiv[0][k] = 0;
 
 			} else {
-				while (massiiv[(-elem % 200)+1][k] != 0){
+				int indeks;
+				if (elem > 0) {                        // Hoolitseb selle eest, et indeks tuleks positiivne
+					indeks = (elem % 300) + 1;
+				} else {
+					indeks = (-elem % 300) + 1;
+				}
+
+				while (massiiv[indeks][k] != 0) {    // Kannab elemndi esimesele vabale kohale
 					k++;
 				}
-				massiiv[(-elem % 200)+1][k] = elem;
-			}
 
+				massiiv[indeks][k] = elem;
+			}
 		}
 
 		return massiiv;
 
 	}
 
-	private static void quicksort(int[] massiiv, int low, int high) {
-		int i = low, j = high;
-		int pivot = massiiv[low + (high-low)/2];
+	/**
+	 * Quicksort algoritmiga mittekahanev massiivi sortimis funktsioon
+	 *
+	 * @param massiiv   Antud massiiv
+	 * @param minIndeks Väikseim sorteerimata indeks massiivis
+	 * @param maxIndeks Suurim sorteerimata indeks massiivis
+	 */
+	public static void quicksort(int[] massiiv, int minIndeks, int maxIndeks) {
+		int i1 = minIndeks;
+		int i2 = maxIndeks;
+		int keskElem = massiiv[minIndeks + (maxIndeks - minIndeks) / 2];    // Leiab massiivi keskel oleva elemendi
+		int x;
 
-		while (i <= j) {
-			while (massiiv[i] < pivot) {
-				i++;
+		while (i1 <= i2) {            // Sorteerib kuni väikseim ja suurim indeks mööduvad
+			while (massiiv[i2] > keskElem) {    // Leiab massiivi lõpust keskmisest elemendist väiksema elemendi
+				i2--;
+			}
+			while (massiiv[i1] < keskElem) {    // Leiab massiivi algusest keskmisest elemendist suurema elemendi
+				i1++;
 			}
 
-			while (massiiv[j] > pivot) {
-				j--;
-			}
-			if (i <= j) {
-				int temp = massiiv[i];
-				massiiv[i] = massiiv[j];
-				massiiv[j] = temp;
-				i++;
-				j--;
+			if (i1 <= i2) {                // Kui algusepoolne element on suurem kui lõpupoolne, siis nende kohad vahetatakse
+				x = massiiv[i1];
+				massiiv[i1] = massiiv[i2];
+				massiiv[i2] = x;
+
+				i2--;
+				i1++;
 			}
 		}
-		if (low < j)
-			quicksort(massiiv, low, j);
-		if (i < high)
-			quicksort(massiiv, i, high);
+
+		if (minIndeks < i2) {                    // Kui massiiv ei ole sorteeritud, siis minnakse uuele ringile
+			quicksort(massiiv, minIndeks, i2);
+		}
+		if (i1 < maxIndeks) {
+			quicksort(massiiv, i1, maxIndeks);
+		}
 	}
 
 	public static void main(String[] args) {
-		int[][] a = new int[3000][3000];
-		for (int i = 0; i < 3000; i++) {
-			for (int j = 0; j < 3000; j++) {
-				a[i][j] = (int) (Math.random()*300+1);
-			}
-		}
-
-		long start = System.nanoTime();
-		int[] arr = korduvadRead(a);
-
-		System.out.println((System.nanoTime() - start)/1000000);
-		System.out.println(arr.length);
-		System.out.println(Kodu3b.korduvadRead(a).length);
 	}
 }
---
+++ src/aeglane.java
@@ -0,0 +1,70 @@
+public class aeglane {
+
+	/**
+	 * Leiab kõik elemendid maatriksist, mis esinevad igal real
+	 *
+	 * @param a Antud maatriks
+	 * @return Mittekahanev massiiv, kus on kõik korduvad arvud ning kui arvu on igal real mitu, siis on teda ka massiivis nii palju
+	 */
+	public static int[] korduvadRead(int[][] a) {
+		int[] korduvad = new int[3000];		// Kasutatkse kandidaatide kontrollimisel puhvrina
+		int loendur = 0;					// Loeb massiivis sobilike kandidaate
+		int pikkus = a.length;
+
+		Kodu3b.quicksort(a[0], 0, pikkus - 1);	// Sorteerib maatriksi esimese rea mittekahanevaks
+
+		int jarjest = 1;	// Loeb mitu ühe väärtusega kandidaati on
+		double eelmine_elem = 0.5;	// Kasutatakse võrdlemiseks, et kas mitu sama väärtusega elementi on järjest
+		int[] kandidaadid = a[0];	// Sobivad kandidaadid
+
+		for (int i = 1; i < pikkus; i++) { 	// Käiakse läbi kõik massiivid
+			for (int elem : kandidaadid) {
+				if (elem == eelmine_elem) {	// Loeb mitmest järjestust tuleb kontrollida
+					jarjest++;
+				} else {
+					jarjest = 1;
+				}
+
+				if (sisaldab(a[i], elem, jarjest)) {	// Kui element on massiivis, siis kantakse puhvrisse
+
+					korduvad[loendur] = elem;
+					loendur++;
+				}
+				eelmine_elem = elem;
+			}
+
+			kandidaadid = new int[loendur];		// Võtab andmed puhvrist ning uuendab kandidaate
+			System.arraycopy(korduvad, 0, kandidaadid, 0, loendur);
+			loendur = 0;
+			eelmine_elem = 0.5;
+		}
+
+
+		return kandidaadid;
+	}
+
+	/**
+	 * Kontrollib, kas element või elementide hulk on massiivis
+	 *
+	 * @param massiiv Antud massiiv.
+	 * @param elem    Antud element.
+	 * @param jarjest Elemendi korduste arv.
+	 * @return Kas element või elemntide hulk oli massiivis.
+	 */
+	public static boolean sisaldab(int[] massiiv, int elem, int jarjest) {
+		for (int j : massiiv) {		// Käib läbi iga elemendi massiivis
+			if (j == elem) {
+				if (jarjest == 1) {	// Kui elemente peab olema rohkem kui 1, siis otsib edasi
+					return true;
+				} else {
+					jarjest--;
+				}
+			}
+		}
+		return false;
+	}
+
+
+	public static void main(String[] args) {
+	}
+}
