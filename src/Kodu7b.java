0d6767d73102bba9fc56f1d90b14d1cb4f19c2d6
Gregor Rammal <gregor.rammal@gmail.com>
2023-04-09 00:13:09 +0300

    Kodutöö 7

--- src/Kodu7b.java
+++ src/Kodu7b.java
@@ -30,55 +30,81 @@ class Kodu7b {
 
 
 		if (kuningaid == 0){
-			return kontroll(laud);
+			for (int[] ints : laud) {
+				System.out.println(Arrays.toString(ints));
+			}
+			System.out.println();
+			return 1;
 		}
 
 		if (rida == laud.length){
-			if (kuningaid == 1){
-				laud[rida1][veerg1] = 1;
-				int tulemus = kontroll(laud);
-				laud[rida1][veerg1] = 0;
-				return tulemus;
+			if (kuningaid == 1 && kontroll(laud, veerg1, rida1)){
+				return 1;
 			}
 
 			return 0;
 		}
 
-		laud[rida1][veerg1] = 1;
-		long tulemus1 = katsed(laud, kuningaid-1, rida, veerg);
 
-		laud[rida1][veerg1] = 0;
+		long tulemus1 = 0;
+		if (kontroll(laud, veerg1, rida1)){
+			laud[rida1][veerg1] = 1;
+			tulemus1 = katsed(laud, kuningaid-1, rida, veerg);
+			laud[rida1][veerg1] = 0;
+		}
+
 		long tulemus2 = katsed(laud, kuningaid, rida, veerg);
 
 		return tulemus1+tulemus2;
 
 	}
 
-	public static int kontroll(int[][] laud){
-
-
-		for (int i = 0; i < laud.length; i++) {
-			for (int j = 0; j < laud[i].length; j++) {
-				if (laud[i][j] == 1){
-					for (int k = -1; k < 2; k++) {
-						for (int l = -1; l < 2; l++) {
-							if (!(l == 0 && k == 0) && (k+i < laud.length && l+j < laud[i].length) &&
-									(k+i > -1 && l+j > -1)){
-								if (laud[k+i][l+j] == 1){
-									return 0;
-								}
-							}
-						}
-					}
-				}
-			}
+	public static boolean kontroll(int[][] laud, int rida, int veerg){
+		if (veerg == 0 && rida == 0){
+			return true;
 		}
-		loendur++;
-		return 1;
+
+		try{
+
+			if (veerg == 0){
+				System.out.println(laud[rida - 1][veerg] != 1 && laud[rida - 1][veerg + 1] != 1);
+			}else
+
+			if (rida == 0){
+				System.out.println( laud[rida][veerg - 1] != 1);
+			}else
+
+			if (veerg == laud[0].length-1){
+				System.out.println(laud[rida - 1][veerg - 1] != 1 && laud[rida - 1][veerg] != 1 && laud[rida][veerg - 1] != 1);
+			}else
+
+				System.out.println( laud[rida - 1][veerg] != 1 && laud[rida][veerg - 1] != 1
+					&& laud[rida - 1][veerg - 1] != 1 && laud[rida - 1][veerg + 1] != 1);
+
+		}catch (Exception e){
+
+		}
+
+		if (veerg == 0){
+			return laud[rida - 1][veerg] != 1 && laud[rida - 1][veerg + 1] != 1;
+		}
+
+		if (rida == 0){
+			return laud[rida][veerg - 1] != 1;
+		}
+
+		if (veerg == laud[0].length-1){
+			return laud[rida - 1][veerg - 1] != 1 && laud[rida - 1][veerg] != 1 && laud[rida][veerg - 1] != 1;
+		}
+
+		return laud[rida - 1][veerg] != 1 && laud[rida][veerg - 1] != 1
+				&& laud[rida - 1][veerg - 1] != 1 && laud[rida - 1][veerg + 1] != 1;
 	}
 
     public static void main(String[] args) {
+		long s = System.currentTimeMillis();
 		System.out.println(kuningad(3, 3));
+		System.out.println(System.currentTimeMillis() - s);
 //		System.out.println(loendur);
 
     }//peameetod
